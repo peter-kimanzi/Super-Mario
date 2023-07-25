25d9c5681af5185af6dfd44433e0843f4b258b38
peter kimanzi <kimjayden001@gmail.com>
2023-07-25 08:50:06 +0300

    js edits

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 const botaoTrailer = document.querySelector(".botao-trailer");
 const botaoFecharModal = document.querySelector(".fechar-modal");
 const video = document.getElementById("video");
-
+const modal = document.querySelector(".modal");
 
